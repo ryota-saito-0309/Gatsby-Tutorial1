e0dca5c86304e14f4b5e49e556105a86b16d8352
Ryota Saito <sa110.instagram@gmail.com>
2020-05-30 18:30:34 +0900

    画像の表示

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ export default function Home() {
     <div style={{ color: `purple` }}>
       <h1>初めてのGatsby!</h1>
       <p>こんにちは.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
   )
 }

ac77b8b82277ccdef5f8da9f63d95fd06e52fcbe
Ryota Saito <sa110.instagram@gmail.com>
2020-05-30 18:30:12 +0900

    文字の変更

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
 
 export default function Home() {
-  return <div style={{ color: `purple`, fontSize: `72px` }}>スタイル変更!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <h1>初めてのGatsby!</h1>
+      <p>こんにちは.</p>
+    </div>
+  )
 }

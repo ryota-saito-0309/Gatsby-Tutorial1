80fbfd664270df01aac397ddfab55d43f6d2a469
Ryota Saito <sa110.instagram@gmail.com>
2020-05-30 18:40:27 +0900

    内容変更

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,8 +3,8 @@ import React from "react"
 export default function About() {
   return (
     <div style={{ color: `teal` }}>
-      <h1>About Gatsby</h1>
-      <p>Such wow. Very React.</p>
+      <h1>Aboutページ</h1>
+      <p>簡単</p>
     </div>
   )
 }
